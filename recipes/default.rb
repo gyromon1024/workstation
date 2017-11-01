ef05f69b11857fc3205e64c4649bb03155b32df8
Todd F <tfyffe@erac.com>
2017-11-01 19:35:11 +0000

    Version 0.1.1, enabled default.rb functionality, but disable-uac.rb can still be called directly

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,5 @@
 # Cookbook:: workstation
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Todd F, All Rights Reserved.
+include_recipe 'workstation::disable-uac'
